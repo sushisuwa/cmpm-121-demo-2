7ad189ce93880db5ccc0ece68102c61ed592b115
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2024-12-03 12:40:08 -0800

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import "./style.css";
 
 const APP_NAME = "Stickerpad Fun";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const game_name = document.createElement("h1");
+game_name.innerText = APP_NAME;
+app.appendChild(game_name);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+const ctx = canvas.getContext("2d");
+if (ctx) {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+app.appendChild(canvas);
