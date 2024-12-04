e1ccf844e32cfb3279fd857704e1c09edd9ab000
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2024-12-04 12:26:27 -0800

    Step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,39 @@ if (ctx) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 app.appendChild(canvas);
+
+const clearButton = document.createElement("button");
+clearButton.innerText = "clear";
+clearButton.addEventListener("click", () => {
+  if (ctx) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+});
+app.appendChild(clearButton);
+
+let isDrawing = false;
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  if (ctx) {
+    ctx.moveTo(e.offsetX, e.offsetY);
+    ctx.beginPath();
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  if (ctx) ctx.closePath();
+});
+
+canvas.addEventListener("mouseout", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    if (ctx) {
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+    }
+  }
+});
